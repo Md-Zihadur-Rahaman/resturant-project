12de452eb6f9d337046842eed388348a7b1e860f
Md Zihadur Rahaman <zihadrahman3@gmail.com>
2024-09-21 19:23:47 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      CormorantInfant: 'Cormorant Infant',
+      JosefinSans: 'Josefin Sans',
+    },
+    colors: {
+      yellow_dark: '#E1B168',
+      black_dark: '#292E36',
+      black_modified: '#343942',
+      black_light: '#555555',
+      cream: '#FFF8F5',
+      blackText: '#292E36',
+      whiteText: '#FFFFFF',
+      offWhiteText: '#DEDEDE',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
